78521085bd05cc90f413ba6695a2b14f58b9b29c
dsan22 <jankovicdusan00@gmail.com>
2024-04-23 00:40:51 +0200

    Added displaying list of ingridents when displaying recipe

--- app/Http/Resources/IngredientResource.php
+++ app/Http/Resources/IngredientResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class RecipeResource extends JsonResource
+class IngredientResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,9 +17,6 @@ class RecipeResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'instructions' => $this->instructions,
-            'ingredients'=> IngredientResource::collection($this->ingredients),
-            'category' => $this->category->name,
             'created_at' => $this->created_at,
             'updated_at'=> $this->updated_at ,
         ];
